bc2b15b10bd009bcdf3c59b69c36bcf0c2b80636
Kishore-89-kis <pkishore255@gmail.com>
2025-02-17 13:04:53 +0530

    locals

---
+++ Locals/local.tf
@@ -0,0 +1,8 @@
+
+locals {
+  ami_id = "ami-09c813fb71547fc4f"
+  instance_type ="t3.micro"
+  
+    name = "${var.environment}-${var.component}-${var.project}"
+
+  }
---
+++ Locals/variable.tf
@@ -0,0 +1,19 @@
+variable name {
+  type        = string
+  default     = "t3.micro"
+  description = "description"
+}
+ variable "project" {
+    default = "expense"
+
+ }
+ 
+ variable "component"{
+    default = "backend"
+ }
+ variable "environment"{
+    default = "dev"
+ }
+
+ 
+ 
